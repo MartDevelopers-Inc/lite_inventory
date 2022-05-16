7ea2d97c1ef44178c9a7007e5a255e3eee7b3103
Martin Mbithi <martdevelopers254@gmail.com>
2022-05-16 16:11:16 +0300

    Manage Stores Modals

--- views/main_dashboard_stores.php
+++ views/main_dashboard_stores.php
@@ -134,13 +134,83 @@ require_once('../partials/head.php');
                                                                 <p class="card-text">Store Email: <?php echo $stores['store_email']; ?></p>
                                                                 <p class="card-text">Store Address: <?php echo $stores['store_adr']; ?></p>
                                                                 <p class="card-text">
-                                                                    <span class="badge badge-dim badge-pill badge-outline-warning">Update</span>
-                                                                    <span class="badge badge-dim badge-pill badge-outline-danger">Delete</span>
+                                                                    <a data-toggle="modal" href="#update_store_<?php echo $stores['store_id']; ?>" class="badge badge-dim badge-pill badge-outline-warning">Update</a>
+                                                                    <a data-toggle="modal" href="#delete_store_<?php echo $stores['store_id']; ?>" class="badge badge-dim badge-pill badge-outline-danger">Delete</a>
                                                                 </p>
                                                             </div>
                                                         </div>
                                                     </div>
                                                 </div>
+                                                <!-- Update And Delete Modals -->
+                                                <div class="modal fade" id="update_store_<?php echo $stores['store_id']; ?>">
+                                                    <div class="modal-dialog  modal-lg">
+                                                        <div class="modal-content">
+                                                            <div class="modal-header">
+                                                                <h4 class="modal-title">Fill All Required Fields</h4>
+                                                                <button type="button" class="close" data-dismiss="modal">
+                                                                    <span>&times;</span>
+                                                                </button>
+                                                            </div>
+                                                            <div class="modal-body">
+                                                                <form method="post" enctype="multipart/form-data">
+                                                                    <div class="form-row">
+                                                                        <div class="form-group col-md-12">
+                                                                            <label>Store Name</label>
+                                                                            <input type="text" value="<?php echo $stores['store_name']; ?>" name="store_name" required class="form-control">
+                                                                            <input type="hidden" value="<?php echo $stores['store_id']; ?>" name="store_id" required class="form-control">
+                                                                        </div>
+                                                                        <div class="form-group col-md-12">
+                                                                            <label>Store Email</label>
+                                                                            <input type="text" value="<?php echo $stores['store_email']; ?>" name="store_email" required class="form-control">
+                                                                        </div>
+                                                                        <div class="form-group col-md-12">
+                                                                            <label>Store Address</label>
+                                                                            <textarea type="text" name="store_adr" rows="3" required class="form-control"><?php echo $stores['store_adr']; ?></textarea>
+                                                                        </div>
+                                                                    </div>
+                                                                    <br><br>
+                                                                    <div class="text-right">
+                                                                        <button name="update_store" class="btn btn-primary" type="submit">
+                                                                            Update Store
+                                                                        </button>
+                                                                    </div>
+                                                                </form>
+                                                            </div>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                                <!-- Delete Modal -->
+                                                <div class="modal fade" id="delete_store_<?php echo $stores['store_id']; ?>" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                                                    <div class="modal-dialog modal-dialog-centered" role="document">
+                                                        <div class="modal-content">
+                                                            <div class="modal-header">
+                                                                <h5 class="modal-title" id="exampleModalLabel">CONFIRM DELETE</h5>
+                                                                <button type="button" class="close" data-dismiss="modal">
+                                                                    <span>&times;</span>
+                                                                </button>
+                                                            </div>
+                                                            <form method="POST">
+                                                                <div class="modal-body text-center">
+                                                                    <h4 class="text-danger">
+                                                                        Delete <?php echo $stores['store_name']; ?> ?
+                                                                        <hr>
+                                                                        This operation is irreversible. Please confirm your password before closing above store
+                                                                    </h4>
+                                                                    <br>
+                                                                    <!-- Hide This -->
+                                                                    <input type="hidden" name="store_id" value="<?php echo $stores['store_id']; ?>">
+                                                                    <input type="hidden" name="store_name" value="<?php echo $stores['store_name']; ?>">
+                                                                    <div class="form-group col-md-12">
+                                                                        <input type="password" required name="user_password" class="form-control">
+                                                                    </div>
+                                                                    <button type="button" class="text-center btn btn-success" data-dismiss="modal">No</button>
+                                                                    <input type="submit" name="delete_store" value="Delete" class="text-center btn btn-danger">
+                                                                </div>
+                                                            </form>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                                <!-- End Modals -->
                                             <?php }
                                         } else { ?>
                                         <?php } ?>
