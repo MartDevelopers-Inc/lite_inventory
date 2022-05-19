4829328957d87eb1a8d8e16cc7291bf3e45bd314
Martin Mbithi <martdevelopers254@gmail.com>
2022-05-19 18:36:17 +0300

    Implement Main Dashboard System Logs

--- views/main_dashboard_system_log_pdf_dump.php
+++ views/main_dashboard_system_log_pdf_dump.php
@@ -74,12 +74,15 @@ $start = date('Y-m-d g:ia', strtotime($_GET['from']));
 $end = date('Y-m-d g:ia', strtotime($_GET['to']));
 $log_type = $_GET['log_type'];
 
-$ret = "SELECT * FROM store_settings  WHERE store_status  = 'active'";
-$stmt = $mysqli->prepare($ret);
-$stmt->execute(); //ok
-$res = $stmt->get_result();
-while ($stores = $res->fetch_object()) {
-    $html = '
+
+/* Load All Logs */
+if ($log_type == 'All Logs') {
+    $ret = "SELECT * FROM store_settings  WHERE store_status  = 'active'";
+    $stmt = $mysqli->prepare($ret);
+    $stmt->execute(); //ok
+    $res = $stmt->get_result();
+    while ($stores = $res->fetch_object()) {
+        $html = '
     <!DOCTYPE html>
     <html>
 
@@ -179,9 +182,157 @@ while ($stores = $res->fetch_object()) {
             <table border="1" cellspacing="0" width="98%" style="font-size:9pt">
                 <thead>
                     <tr>
-                        <th style="width:90%">User Details</th>
-                        <th style="width:50%">IP Address</th>
-                        <th style="width:80%">Created On</th>
+                        <th style="width:80%">User Details</th>
+                        <th style="width:40%">IP Address</th>
+                        <th style="width:70%">Created On</th>
+                        <th style="width:100%">Log Details</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    ';
+                    $ret = "SELECT * FROM system_logs l INNER JOIN users u
+                    ON u.user_id = l.log_user_id WHERE l.log_created_at BETWEEN '{$start}' AND '{$end}' 
+                    ORDER BY l.log_created_at DESC";
+                    $stmt = $mysqli->prepare($ret);
+                    $stmt->execute(); //ok
+                    $res = $stmt->get_result();
+                    while ($logs = $res->fetch_object()) {
+
+                        $html .=
+                            '
+                                <tr>
+                                    <td>' . $logs->user_name . ' <br> ' . $logs->user_email . '</td>
+                                    <td>' . $logs->log_ip_address . '</td>
+                                    <td>' . date('d M Y g:ia', strtotime($logs->log_created_at)) . '</td>
+                                    <td>' . $logs->log_details . '</td>
+                                </tr>
+                            ';
+                    }
+                    $html .= '
+                </tbody>
+            </table>
+        </body>
+    </html>';
+
+        $dompdf = new Dompdf();
+        $dompdf->load_html($html);
+        $dompdf->set_paper('A4');
+        $dompdf->set_option('isHtml5ParserEnabled', true);
+        $dompdf->render();
+        $dompdf->stream('System Logs From ' . $start . ' To ' . $end, array("Attachment" => 1));
+        $options = $dompdf->getOptions();
+        $options->setDefaultFont('');
+        $dompdf->setOptions($options);
+    }
+}else{
+    $ret = "SELECT * FROM store_settings  WHERE store_status  = 'active'";
+    $stmt = $mysqli->prepare($ret);
+    $stmt->execute(); //ok
+    $res = $stmt->get_result();
+    while ($stores = $res->fetch_object()) {
+        $html = '
+    <!DOCTYPE html>
+    <html>
+
+        <head>
+            <meta name="" content="XYZ,0,0,1" />
+            <style type="text/css">
+                table {
+                    font-size: 12px;
+                    padding: 4px;
+                }
+
+                tr {
+                    page-break-after: always;
+                }
+
+                th {
+                    text-align: left;
+                    padding: 4pt;
+                }
+
+                td {
+                    padding: 5pt;
+                }
+
+                #b_border {
+                    border-bottom: dashed thin;
+                }
+
+                legend {
+                    color: #0b77b7;
+                    font-size: 1.2em;
+                }
+
+                #error_msg {
+                    text-align: left;
+                    font-size: 11px;
+                    color: red;
+                }
+
+                .header {
+                    margin-bottom: 20px;
+                    width: 100%;
+                    text-align: left;
+                    position: absolute;
+                    top: 0px;
+                }
+
+                .footer {
+                    width: 100%;
+                    text-align: center;
+                    position: fixed;
+                    bottom: 5px;
+                }
+
+                #no_border_table {
+                    border: none;
+                }
+
+                #bold_row {
+                    font-weight: bold;
+                }
+
+                #amount {
+                    text-align: right;
+                    font-weight: bold;
+                }
+
+                .pagenum:before {
+                    content: counter(page);
+                }
+
+                /* Thick red border */
+                hr.red {
+                    border: 1px solid red;
+                }
+                .list_header{
+                    font-family: "Helvetica Neue", "Helvetica", Helvetica, Arial, sans-serif;
+                }
+            </style>
+        </head>
+
+        <body style="margin:1px;">
+            <div class="footer">
+                <hr>
+                <i>' . $log_type . ' . Report Generated On ' . date('d M Y') . '</i>
+            </div>
+
+            <div class="list_header" align="center">
+                <h3>
+                    ' . $stores->store_name . ' <br>
+                    ' . $stores->store_email . ' <br>
+                    ' . $stores->store_adr . '  
+                </h3>
+                <hr style="width:100%" , color=black>
+                <h4> ' . $log_type . ' From ' . date('d M Y', strtotime($start)) . ' To ' . date('d M Y', strtotime($end)) . ' </h4>
+            </div>
+            <table border="1" cellspacing="0" width="98%" style="font-size:9pt">
+                <thead>
+                    <tr>
+                        <th style="width:80%">User Details</th>
+                        <th style="width:40%">IP Address</th>
+                        <th style="width:70%">Created On</th>
                         <th style="width:100%">Log Details</th>
                     </tr>
                 </thead>
@@ -212,13 +363,14 @@ while ($stores = $res->fetch_object()) {
         </body>
     </html>';
 
-    $dompdf = new Dompdf();
-    $dompdf->load_html($html);
-    $dompdf->set_paper('A4');
-    $dompdf->set_option('isHtml5ParserEnabled', true);
-    $dompdf->render();
-    $dompdf->stream('System Logs From ' . $start . ' To ' . $end, array("Attachment" => 1));
-    $options = $dompdf->getOptions();
-    $options->setDefaultFont('');
-    $dompdf->setOptions($options);
+        $dompdf = new Dompdf();
+        $dompdf->load_html($html);
+        $dompdf->set_paper('A4');
+        $dompdf->set_option('isHtml5ParserEnabled', true);
+        $dompdf->render();
+        $dompdf->stream($log_type . ' From ' . $start . ' To ' . $end, array("Attachment" => 1));
+        $options = $dompdf->getOptions();
+        $options->setDefaultFont('');
+        $dompdf->setOptions($options);
+    }
 }
