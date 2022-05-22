ed8329b0c14a56f3fa8ac6f90953e428fa19f9dd
Martin Mbithi <martdevelopers254@gmail.com>
2022-05-22 10:08:39 +0300

    Craft POS Modue Landing Page

--- views/pos.php
+++ views/pos.php
@@ -80,53 +80,11 @@ require_once('../partials/head.php');
                             <div class="nk-block-head">
                                 <div class="nk-block-between-md g-3">
                                     <div class="nk-block-head-content">
-                                        <div class="nk-block-head-sub"><span>Welcome!</span></div>
                                         <div class="align-center flex-wrap pb-2 gx-4 gy-3">
                                             <div>
-                                                <h2 class="nk-block-title fw-normal">Abu Bin Ishtiyak</h2>
+                                                <h2 class="nk-block-title fw-normal">POS Module</h2>
                                             </div>
-                                            <div><a href="html/invest/schemes.html" class="btn btn-white btn-light">My Plans <em class="icon ni ni-arrow-long-right ml-2"></em></a></div>
                                         </div>
-                                        <div class="nk-block-des">
-                                            <p>At a glance summary of your investment account. Have fun!</p>
-                                        </div>
-                                    </div><!-- .nk-block-head-content -->
-                                    <div class="nk-block-head-content d-none d-md-block">
-                                        <div class="nk-slider nk-slider-s1">
-                                            <div class="slider-init" data-slick='{"dots": true, "arrows": false, "fade": true}'>
-                                                <div class="slider-item">
-                                                    <div class="nk-iv-wg1">
-                                                        <div class="nk-iv-wg1-sub sub-text">My Active Plans</div>
-                                                        <h6 class="nk-iv-wg1-info title">Silver - 4.76% for 21 Days</h6>
-                                                        <a href="#" class="nk-iv-wg1-link link link-light"><em class="icon ni ni-trend-up"></em> <span>Check Details</span></a>
-                                                        <div class="nk-iv-wg1-progress">
-                                                            <div class="progress-bar bg-primary" data-progress="80"></div>
-                                                        </div>
-                                                    </div>
-                                                </div><!-- .slider-item -->
-                                                <div class="slider-item">
-                                                    <div class="nk-iv-wg1">
-                                                        <div class="nk-iv-wg1-sub sub-text">My Active Plans</div>
-                                                        <h6 class="nk-iv-wg1-info title">Silver - 4.76% for 21 Days</h6>
-                                                        <a href="#" class="nk-iv-wg1-link link link-light"><em class="icon ni ni-trend-up"></em> <span>Check Details</span></a>
-                                                        <div class="nk-iv-wg1-progress">
-                                                            <div class="progress-bar bg-primary" data-progress="80"></div>
-                                                        </div>
-                                                    </div>
-                                                </div><!-- .slider-item -->
-                                                <div class="slider-item">
-                                                    <div class="nk-iv-wg1">
-                                                        <div class="nk-iv-wg1-sub sub-text">My Active Plans</div>
-                                                        <h6 class="nk-iv-wg1-info title">Silver - 4.76% for 21 Days</h6>
-                                                        <a href="#" class="nk-iv-wg1-link link link-light"><em class="icon ni ni-trend-up"></em> <span>Check Details</span></a>
-                                                        <div class="nk-iv-wg1-progress">
-                                                            <div class="progress-bar bg-primary" data-progress="80"></div>
-                                                        </div>
-                                                    </div>
-                                                </div><!-- .slider-item -->
-                                            </div>
-                                            <div class="slider-dots"></div>
-                                        </div><!-- .nk-slider -->
                                     </div><!-- .nk-block-head-content -->
                                 </div><!-- .nk-block-between -->
                             </div><!-- .nk-block-head -->
