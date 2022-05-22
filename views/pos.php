6c65afb5389cdfced68ba2cb54e6b0b21c8c8497
Martin Mbithi <martdevelopers254@gmail.com>
2022-05-22 10:15:14 +0300

    Implement POS - Search Products

--- views/pos.php
+++ views/pos.php
@@ -89,74 +89,37 @@ require_once('../partials/head.php');
                                 </div><!-- .nk-block-between -->
                             </div><!-- .nk-block-head -->
                             <div class="nk-block">
-                                <div class="nk-news card card-bordered">
+                                <div class="nk-news card card-bordered border border-success">
                                     <div class="card-inner">
                                         <div class="nk-news-list">
-                                            <a class="nk-news-item" href="#">
-                                                <div class="nk-news-icon">
-                                                    <em class="icon ni ni-card-view"></em>
+                                            <form class="inline" method="post">
+                                                <div class="form-group col-md-8">
+                                                    <select data-search="on" class="form-select form-control form-control-lg " name="querry" type="search">
+                                                        <option>Select Product To Add To Cart</option>
+                                                        <?php
+                                                        $ret = "SELECT * FROM products 
+                                                            WHERE product_status = 'active'
+                                                            ORDER BY product_name ASC";
+                                                        $stmt = $mysqli->prepare($ret);
+                                                        $stmt->execute(); //ok
+                                                        $res = $stmt->get_result();
+                                                        while ($products = $res->fetch_object()) {
+                                                        ?>
+                                                            <option value="<?php echo $products->product_id; ?>"><?php echo $products->product_code . ' ' . $products->product_name; ?></option>
+                                                        <?php } ?>
+                                                    </select>
+                                                    <div class="text-center">
+                                                        <button name="search" class="text-center btn btn-outline-success my-2 my-sm-0" type="submit"><i class="fas fa-search"></i>Search</button>
+                                                    </div>
                                                 </div>
-                                                <div class="nk-news-text">
-                                                    <p>Do you know the latest update of 2019? <span> A overview of our is now available on YouTube</span></p>
-                                                    <em class="icon ni ni-external"></em>
-                                                </div>
-                                            </a>
+                                            </form>
                                         </div>
                                     </div>
                                 </div><!-- .card -->
                             </div><!-- .nk-block -->
                             <div class="nk-block">
                                 <div class="row gy-gs">
-                                    <div class="col-md-6 col-lg-4">
-                                        <div class="nk-wg-card is-dark card card-bordered">
-                                            <div class="card-inner">
-                                                <div class="nk-iv-wg2">
-                                                    <div class="nk-iv-wg2-title">
-                                                        <h6 class="title">Available Balance <em class="icon ni ni-info"></em></h6>
-                                                    </div>
-                                                    <div class="nk-iv-wg2-text">
-                                                        <div class="nk-iv-wg2-amount"> 105.94 <span class="change up"><span class="sign"></span>3.4%</span>
-                                                        </div>
-                                                    </div>
-                                                </div>
-                                            </div>
-                                        </div><!-- .card -->
-                                    </div><!-- .col -->
-                                    <div class="col-md-6 col-lg-4">
-                                        <div class="nk-wg-card is-s1 card card-bordered">
-                                            <div class="card-inner">
-                                                <div class="nk-iv-wg2">
-                                                    <div class="nk-iv-wg2-title">
-                                                        <h6 class="title">Total Invested <em class="icon ni ni-info"></em></h6>
-                                                    </div>
-                                                    <div class="nk-iv-wg2-text">
-                                                        <div class="nk-iv-wg2-amount"> 509,850.90 <span class="change up"><span class="sign"></span>2.8%</span>
-                                                        </div>
-                                                    </div>
-                                                </div>
-                                            </div>
-                                        </div><!-- .card -->
-                                    </div><!-- .col -->
-                                    <div class="col-md-12 col-lg-4">
-                                        <div class="nk-wg-card is-s3 card card-bordered">
-                                            <div class="card-inner">
-                                                <div class="nk-iv-wg2">
-                                                    <div class="nk-iv-wg2-title">
-                                                        <h6 class="title">Total Profits <em class="icon ni ni-info"></em></h6>
-                                                    </div>
-                                                    <div class="nk-iv-wg2-text">
-                                                        <div class="nk-iv-wg2-amount"> 50,600.48 <span class="change down"><span class="sign"></span>1.4%</span>
-                                                        </div>
-                                                    </div>
-                                                </div>
-                                            </div>
-                                        </div><!-- .card -->
-                                    </div><!-- .col -->
-                                </div><!-- .row -->
-                            </div><!-- .nk-block -->
-                            <div class="nk-block">
-                                <div class="row gy-gs">
-                                    <div class="col-md-6 col-lg-4">
+                                    <div class="col-md-12 col-lg-12">
                                         <div class="nk-wg-card card card-bordered h-100">
                                             <div class="card-inner h-100">
                                                 <div class="nk-iv-wg2">
@@ -188,135 +151,7 @@ require_once('../partials/head.php');
                                             </div>
                                         </div><!-- .card -->
                                     </div><!-- .col -->
-                                    <div class="col-md-6 col-lg-4">
-                                        <div class="nk-wg-card card card-bordered h-100">
-                                            <div class="card-inner h-100">
-                                                <div class="nk-iv-wg2">
-                                                    <div class="nk-iv-wg2-title">
-                                                        <h6 class="title">This Month Profit <em class="icon ni ni-info text-primary"></em></h6>
-                                                    </div>
-                                                    <div class="nk-iv-wg2-text">
-                                                        <div class="nk-iv-wg2-amount ui-v2">1,457.23 <span class="change up"><span class="sign"></span>4.5%</span></div>
-                                                        <ul class="nk-iv-wg2-list">
-                                                            <li>
-                                                                <span class="item-label">Profits</span>
-                                                                <span class="item-value">1,045.21</span>
-                                                            </li>
-                                                            <li>
-                                                                <span class="item-label">Referrals</span>
-                                                                <span class="item-value">212.02</span>
-                                                            </li>
-                                                            <li>
-                                                                <span class="item-label">Rewards</span>
-                                                                <span class="item-value">200.00</span>
-                                                            </li>
-                                                            <li class="total">
-                                                                <span class="item-label">Total Profit</span>
-                                                                <span class="item-value">1,457.23</span>
-                                                            </li>
-                                                        </ul>
-                                                    </div>
-                                                    <div class="nk-iv-wg2-cta">
-                                                        <a href="#" class="btn btn-primary btn-lg btn-block">Invest & Earn</a>
-                                                        <div class="cta-extra">Earn up to 25$ <a href="#" class="link link-dark">Refer friend!</a></div>
-                                                    </div>
-                                                </div>
-                                            </div>
-                                        </div><!-- .card -->
-                                    </div><!-- .col -->
-                                    <div class="col-md-12 col-lg-4">
-                                        <div class="nk-wg-card card card-bordered h-100">
-                                            <div class="card-inner h-100">
-                                                <div class="nk-iv-wg2">
-                                                    <div class="nk-iv-wg2-title">
-                                                        <h6 class="title">My Investment</h6>
-                                                    </div>
-                                                    <div class="nk-iv-wg2-text">
-                                                        <div class="nk-iv-wg2-amount ui-v2">319 <span class="sub">03</span> Active</div>
-                                                        <ul class="nk-iv-wg2-list">
-                                                            <li>
-                                                                <span class="item-label"><a href="#">Silver</a> <small>- 4.76% for 21 Days</small></span>
-                                                                <span class="item-value">2,500.00</span>
-                                                            </li>
-                                                            <li>
-                                                                <span class="item-label"><a href="#">Silver</a> <small>- 4.76% for 21 Days</small></span>
-                                                                <span class="item-value">2,000.00</span>
-                                                            </li>
-                                                            <li>
-                                                                <span class="item-label"><a href="#">Dimond</a> <small>- 14.29% for 14 Days</small></span>
-                                                                <span class="item-value">8,000.00</span>
-                                                            </li>
-                                                            <li>
-                                                                <span class="item-label"><a href="#">Starter</a> <small>- 1.67% for 30 Days</small></span>
-                                                                <span class="item-value">335.00</span>
-                                                            </li>
-                                                        </ul>
-                                                    </div>
-                                                    <div class="nk-iv-wg2-cta">
-                                                        <a href="#" class="btn btn-light btn-lg btn-block">See all Investment</a>
-                                                        <div class="cta-extra">Check out <a href="#" class="link link-dark">Analytic Report</a></div>
-                                                    </div>
-                                                </div>
-                                            </div>
-                                        </div><!-- .card -->
-                                    </div><!-- .col -->
                                 </div><!-- .row -->
-                            </div><!-- .nk-block -->
-                            <div class="nk-block">
-                                <div class="card card-bordered">
-                                    <div class="nk-refwg">
-                                        <div class="nk-refwg-invite card-inner">
-                                            <div class="nk-refwg-head g-3">
-                                                <div class="nk-refwg-title">
-                                                    <h5 class="title">Refer Us & Earn</h5>
-                                                    <div class="title-sub">Use the bellow link to invite your friends.</div>
-                                                </div>
-                                                <div class="nk-refwg-action">
-                                                    <a href="#" class="btn btn-primary">Invite</a>
-                                                </div>
-                                            </div>
-                                            <div class="nk-refwg-url">
-                                                <div class="form-control-wrap">
-                                                    <div class="form-clip clipboard-init" data-clipboard-target="#refUrl" data-success="Copied" data-text="Copy Link"><em class="clipboard-icon icon ni ni-copy"></em> <span class="clipboard-text">Copy Link</span></div>
-                                                    <div class="form-icon">
-                                                        <em class="icon ni ni-link-alt"></em>
-                                                    </div>
-                                                    <input type="text" class="form-control copy-text" id="refUrl" value="https://dashlite.net/?ref=4945KD48">
-                                                </div>
-                                            </div>
-                                        </div>
-                                        <div class="nk-refwg-stats card-inner bg-lighter">
-                                            <div class="nk-refwg-group g-3">
-                                                <div class="nk-refwg-name">
-                                                    <h6 class="title">My Referral <em class="icon ni ni-info" data-toggle="tooltip" data-placement="right" title="Referral Informations"></em></h6>
-                                                </div>
-                                                <div class="nk-refwg-info g-3">
-                                                    <div class="nk-refwg-sub">
-                                                        <div class="title">394</div>
-                                                        <div class="sub-text">Total Joined</div>
-                                                    </div>
-                                                    <div class="nk-refwg-sub">
-                                                        <div class="title">548.49</div>
-                                                        <div class="sub-text">Referral Earn</div>
-                                                    </div>
-                                                </div>
-                                                <div class="nk-refwg-more dropdown mt-n1 mr-n1">
-                                                    <a href="#" class="btn btn-icon btn-trigger" data-toggle="dropdown"><em class="icon ni ni-more-h"></em></a>
-                                                    <div class="dropdown-menu dropdown-menu-xs dropdown-menu-right">
-                                                        <ul class="link-list-plain sm">
-                                                            <li><a href="#">7 days</a></li>
-                                                            <li><a href="#">15 Days</a></li>
-                                                            <li><a href="#">30 Days</a></li>
-                                                        </ul>
-                                                    </div>
-                                                </div>
-                                            </div>
-                                            <div class="nk-refwg-ck">
-                                                <canvas class="chart-refer-stats" id="refBarChart"></canvas>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div><!-- .card -->
                             </div><!-- .nk-block -->
                         </div>
                     </div>
