6e8f9eb0bd763a24b0d0804f7f6d99bef6b35503
Mart Mbithi <martdevelopers254@gmail.com>
2024-09-16 11:37:26 +0300

    Craft Expenses Main Pages

--- helpers/modals/expenses.php
+++ helpers/modals/expenses.php
@@ -38,3 +38,34 @@
         </div>
     </div>
 </div>
+
+<!-- Delete Modal -->
+<div class="modal fade" id="delete_<?php echo $expenses->expense_id; ?>" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog modal-dialog-centered" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="exampleModalLabel">CONFIRM DELETE</h5>
+                <button type="button" class="close" data-dismiss="modal">
+                    <span>&times;</span>
+                </button>
+            </div>
+            <form method="POST">
+                <div class="modal-body text-center ">
+                    <h4 class="text-danger">
+                        Delete <?php echo $expenses->expense_name; ?>'s account? <br>
+                        This operation is delicate and Irreversible.
+                    </h4>
+                    <br>
+                    <!-- Hide This -->
+                    <input type="hidden" name="expense_id" value="<?php echo $expenses->expense_id; ?>">
+                    <input type="hidden" name="expense_name" value="<?php echo $expenses->expense_name; ?>">
+                    <input type="hidden" name="expense_amount" value="<?php echo $expenses->expense_amount; ?>">
+                    <input type="hidden" name="expense_date" value="<?php echo $expenses->expense_date; ?>">
+                    <button type="button" class="text-center btn btn-success" data-dismiss="modal"><em class="icon ni ni-cross-round"></em> No</button>
+                    <button type="submit" name="Delete_Expense" class="text-center btn btn-danger"><em class="icon ni ni-trash-fill"></em> Yes Delete</button>
+                </div>
+            </form>
+        </div>
+    </div>
+</div>
+<!-- End Delete Modal -->
